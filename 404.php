b61d5c1b0cce9daf801a2d0fd401ef27cefaa88e
ielahe <elahemahmoodi81@gmail.com>
2023-09-26 17:40:04 +0330

    404 page created

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<main class="container page-notfound">
+    <div class="not-found-content">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/imgs/404.svg" alt="404" class="image-404">
+        <div class="text-not-found">متاسفانه صفحه مورد نظر یافت نشد</div>
+        <div class="primary-btn"><a href="/">بازگشت به صفحه اصلی</a></div>
+    </div>
+</main>
+<?php get_footer(); ?>
